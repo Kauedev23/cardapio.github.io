856b17c258ac8e2f50104db8036220fb8455e586
Kauê Tadeu <141750656+Kauedev23@users.noreply.github.com>
2024-04-06 00:43:08 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -197,7 +197,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 0.0 && hora <22;
+    return hora >= 18 && hora <22;
     //true = Restaurante aberto
 } 
 //ALTERAR AQUI!
